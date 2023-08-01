9a800d594226b76706c7f2f253c75101ca871690
V-Sugak <vicka.sugak2010@yandex.ru>
2023-08-01 15:54:01 +0300

    hw3

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ export function App() {
         <div className="App">
             <HW1/>
             <HW2 />
-            {/*<HW3 />*/}
+            <HW3 />
             {/*<HW4 />*/}
         </div>
     );
